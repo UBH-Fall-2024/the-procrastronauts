bc0d7f3a9d3384d9985d45c61be7e302321efb42
elliotdelano <75746725+elliotdelano@users.noreply.github.com>
2024-11-09 14:25:07 -0500

    more server stuff

--- back/main.py
+++ back/main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO
 from markupsafe import escape
+import json
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
@@ -22,10 +23,33 @@ def test_connect(auth):
 def test_disconnect():
     print('Client disconnected')
 
-@socketio.on('message')
-def message(data):
+@socketio.on('send')
+def message(content):
+    data = json.loads(content)
+    if data['id'] not in clients:
+        clients[data.id] = (data['loc']['lon'], data['loc']['lat'])
+    out = {}
+    out['from'] = data['id']
+    out['msg'] =  escape(data['msg'])
+    pl = json.dumps(out)
     for i, l in clients.items():
-        socketio.send(f'Anon: {escape(data.msg)}')
+        socketio.emit('recieve', pl)
+
+def client_disconnect(id):
+    socketio.emit('disconnect', to=id)
+    del clients[id]
+
+def client_update():
+    while True:
+        for i, l in clients.items():
+            try:
+                loc = json.loads(socketio.call('status', timeout=120))
+                clients[i] = (loc['lon'], loc['lat'])
+            except TimeoutError:
+                client_disconnect(i)
+            
+        socketio.sleep(3)
 
 if __name__ == '__main__':
+    socketio.start_background_task(client_update)
     socketio.run(app)
