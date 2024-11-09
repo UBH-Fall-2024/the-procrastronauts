41d264ed547cbb5c1138661e642e81b106d7749c
elliotdelano <75746725+elliotdelano@users.noreply.github.com>
2024-11-09 13:17:57 -0500

    basic server stuff

--- back/main.py
+++ back/main.py
@@ -1,9 +1,31 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO
+from markupsafe import escape
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
+
+clients = {}
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@socketio.on('connect')
+def test_connect(auth):
+    print("socket connection")
+    print(auth)
+    socketio.emit('my response', {'data': 'Connected'})
+
+@socketio.on('disconnect')
+def test_disconnect():
+    print('Client disconnected')
+
+@socketio.on('message')
+def message(data):
+    for i, l in clients.items():
+        socketio.send(f'Anon: {escape(data.msg)}')
 
 if __name__ == '__main__':
     socketio.run(app)
